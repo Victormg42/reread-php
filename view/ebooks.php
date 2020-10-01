c3d8f2c268fa0fc8fb3c1163bbfc34aee5048783
Victormg42 <100005779.joan23@fje.edu>
2020-10-01 17:47:52 +0200

    Modificaciones añadiendo el formulario

--- view/ebooks.php
+++ view/ebooks.php
@@ -26,6 +26,37 @@
           <a href="ebooks.php">eBooks</a>
         </div>
     <h3>Toda la actualidad en eBook</h3>
+    <!------Nuevo desarrollo: formulario para filtrar autor-->
+    <div>
+      <form action="ebooks.php" method="POST">
+        <label for="fautor">Autor</label>
+        <input type="text" id="fautor" name="fautor" placeholder="Introduce el autor...">
+
+        <!-- <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="country">Country</label>
+        <select id="country" name="country">
+          <option value="australia">Australia</option>
+          <option value="canada">Canada</option>
+          <option value="usa">USA</option>
+        </select>
+   -->
+        <input type="submit" value="Buscar">
+      </form>
+    </div>
+    <?php
+    if(isset($_POST['fautor'])){
+      //filtrará los ebooks que se mostrarán en la página
+    }else {
+      // Sino mostrará todos los ebooks de la base de datos
+    }
+    ?>
+
+
+
+
+
     <!---
     <div class="gallery">
         <img src="../img/cell.jpeg" alt="Cell">
