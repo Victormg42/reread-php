1b7b511c9a862870d7adca3fe2d803ba47f8117f
Victormg42 <100005779.joan23@fje.edu>
2020-10-02 19:37:25 +0200

    Añadido el formulario en la pagina ebook

--- view/ebooks.php
+++ view/ebooks.php
@@ -27,48 +27,44 @@
         </div>
     <h3>Toda la actualidad en eBook</h3>
     <!------Nuevo desarrollo: formulario para filtrar autor-->
-    <div>
+    <div class="form">
       <form action="ebooks.php" method="POST">
         <label for="fautor">Autor</label>
         <input type="text" id="fautor" name="fautor" placeholder="Introduce el autor...">
 
         <!-- <label for="lname">Last Name</label>
         <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-        <label for="country">Country</label>
+-->
+        <label for="country">País</label>
         <select id="country" name="country">
-          <option value="australia">Australia</option>
-          <option value="canada">Canada</option>
-          <option value="usa">USA</option>
+          <option value="%">Todos los paises</option>
+          <?php
+          // 1. Conexión con la base de datos
+          include '../services/connection.php';
+          $query="SELECT DISTINCT Authors.Country FROM Authors ORDER BY Country";
+          $result=mysqli_query($conn, $query);
+          while ($row = mysqli_fetch_array($result)) {
+            echo '<option value='.$row[Country].'>'.$row[Country].'</option>';
+          }
+          
+          ?>
         </select>
-   -->
         <input type="submit" value="Buscar">
       </form>
     </div>
     <?php
     if(isset($_POST['fautor'])){
       //filtrará los ebooks que se mostrarán en la página
+      $query = "SELECT Books.Description, Books.img, Books.Title FROM Books INNER JOIN BooksAuthors 
+      ON Id=BooksAuthors.BookId INNER JOIN Authors ON Authors.Id = BooksAuthors.AuthorId WHERE Authors.Name 
+      LIKE '%{$_POST['fautor']}%' AND Authors.Country LIKE '%{$_POST['country']}%'";
+      $result = mysqli_query($conn, $query);
+
     }else {
       // Sino mostrará todos los ebooks de la base de datos
-    }
-    ?>
-
-
-
-
-
-    <!---
-    <div class="gallery">
-        <img src="../img/cell.jpeg" alt="Cell">
-        <div class="desc">A través de los teléfonos móviles se envía un mensaje que convierte a todos en esclavos asesinos...</div>
-      </div>
-    -->
-    <?php
-    // 1. Conexión con la base de datos
-    include '../services/connection.php';
-
-    // 2. Selección y muestra de datos de la base de datos
+      // 2. Selección y muestra de datos de la base de datos
     $result = mysqli_query($conn, "SELECT Books.Description, Books.img, Books.Title FROM Books");
+    }
 
     if (!empty($result) && mysqli_num_rows($result) > 0) {
       // datos de salida de cada fila (fila = row)
@@ -81,14 +77,21 @@
         // Añadimos el título a la página con la etiqueta h2 de HTML
         echo "<div class='desc'>".$row['Description']."</div>";
         echo "</div>";
-        if ($i%3==0) {
-          echo "<div style='clear:both;'></div>";
-        }
+          if ($i%3==0) {
+            echo "<div style='clear:both;'></div>";
+          }
       }
-    } else {
-      echo "0 resultados";
-    }
+  } else {
+    echo "0 resultados";
+  }
     ?>
+
+    <!---
+    <div class="gallery">
+        <img src="../img/cell.jpeg" alt="Cell">
+        <div class="desc">A través de los teléfonos móviles se envía un mensaje que convierte a todos en esclavos asesinos...</div>
+      </div>
+    -->
   </div>
   
   <?php
